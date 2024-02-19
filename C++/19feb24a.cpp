4c50dfef9ac84bd22355cfccc2a7bf3ba3f21f21
Aashutosh-Thakur <thakuraashutosh13@gmail.com>
2024-02-19 11:51:49 +0530

    12feb2024

---
+++ C++/19feb24a.cpp
@@ -0,0 +1,14 @@
+//Sum of two number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"Enter another number: ";
+    cin>>b;
+    sum=a+b;
+    cout<<a+b;
+    return 0;
+}
