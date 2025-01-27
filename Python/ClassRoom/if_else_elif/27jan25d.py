e44cd81a58d6b26d66938da5d969cc5b7d9842a2
Aashutosh-Thakur <thakuraashutosh13@gmail.com>
2025-01-27 11:52:24 +0530

    commit

---
+++ Python/ClassRoom/if_else_elif/27jan25d.py
@@ -0,0 +1,7 @@
+price=50
+if price > 100:
+    print("Price is greater than 100")
+elif price == 100:
+    print("Price is 100")
+else:
+    print("Price is less than 100")
---
+++ Python/ClassRoom/if_else_elif/27jan25f.py
@@ -0,0 +1,7 @@
+#Combining conditions with logical operators
+x = 10
+y = 20
+if(x>5 and y>15) or (x<5 and y<15):
+    print("Both conditions are satisfied")
+else:
+    print("At least one condition is not satisfied")
