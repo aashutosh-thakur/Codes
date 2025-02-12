05d094494353ddf26d71f494e5825a4e47228096
Aashutosh-Thakur <thakuraashutosh13@gmail.com>
2025-02-12 16:05:00 +0530

    commit

---
+++ DAA/12feb25a.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+int cut_rod(int p[], int n){
+    if (n==0)
+        return 0;
+    int q=-1e9;
+    for(int i=1;i<=n;i++){
+        q = max(q, p[i-1]+cut_rod(p,n-i));
+    }
+    return q;
+}
+void max(int prices[], int length){
+    int result = cut_rod(prices, length);
+    cout << "Maximum Profit: " << result << endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the length of the rod: ";
+    cin>>n;
+    int* prices = new int[n];
+    cout<<"Enter the prices: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>prices[i];
+    }
+    max(prices ,n);
+    delete[] prices;
+    return 0;
+}
---
+++ Python/Practical/Practical6/12feb25a.py
@@ -0,0 +1,5 @@
+# Online Shopping Cart: Imagine you’re developing an online shopping platform. Create a Python program that simulates a user’s shopping cart.
+# •    Allow the user to add product names and prices to their cart.
+# •    Display the current items in the cart.
+# •    Allow the user to remove items from the cart.
+# •    Calculate the total price and display the total number of items in the cart.
