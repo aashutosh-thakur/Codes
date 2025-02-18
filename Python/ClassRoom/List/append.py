8f99475452d436e34b8aa9eb9124a8d275f2299d
Aashutosh-Thakur <thakuraashutosh13@gmail.com>
2025-02-18 09:34:01 +0530

    commit

---
+++ Python/ClassRoom/List/append.py
@@ -0,0 +1,4 @@
+my_list = ['Mathematics','Chemistry',1997,2000]
+print(my_list)
+my_list.append(20544)
+print(my_list)
---
+++ Python/ClassRoom/List/insert.py
@@ -0,0 +1,6 @@
+my_list=['Mathematics','Chemistry',1997,2000]
+print(my_list)
+my_list.insert(2, 10087)
+my_list.insert(10, 'a')
+my_list.insert(5, 456)
+print(my_list)
