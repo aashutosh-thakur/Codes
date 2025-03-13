9a424f00e9df2b76b2760bda99ec8a742775c567
Aashutosh-Thakur <thakuraashutosh13@gmail.com>
2025-03-13 20:55:04 +0545

    commit

--- Python/Practical/Practical8/12mar25b.py
+++ Python/Practical/Practical8/12mar25b.py
@@ -1,6 +1,8 @@
 # You are working on a text processing tool and need to determine whether a given string is a palindrome. Write a Python function is_palindrome(word) that takes a word as input and returns True if it is a palindrome, otherwise False.
+def is_palindrome(words):
+    if words==words[::-1]:
+        print("True")
+    else:
+        print("False")
 words=input("Enter the word: ")
-if words==words[::-1]:
-    print("True")
-else:
-    print("False")
+is_palindrome(words)
--- Python/Practical/Practical8/13mar25b.py
+++ Python/Practical/Practical8/13mar25b.py
@@ -4,3 +4,17 @@
 # Output: True
 # ("hello", "world")
 # Output: False
+def anagram(word1,word2):
+    if len(word1) == len(word2):
+        for letter in word1:
+            if letter in word2:
+                word2 = word2.replace(letter, "", 1)
+        if len(word2) == 0:
+            print("True")
+        else:
+            print("False")
+    else:
+        print("False")
+word1=input("Enter the first word: ")
+word2=input("Enter the second word: ")
+anagram(word1,word2)
